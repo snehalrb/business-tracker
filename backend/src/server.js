eadbccd8b8f6f53d27c36d46da6a99937f37f9b6
Snehal Bhonsle <snehalrb@gmail.com>
2025-10-29 16:52:27 +1100

    chaging server.js and axios.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,39 @@ import router from "./routes/routes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+const __dirname = path.resolve();
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
 
 app.use(express.json());
 
 app.use("/business-tracker", router);
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
+
+if (process.env.NODE_ENV === "production") {
+  // Absolute path to frontend dist folder
+  const frontendPath = path.join(__dirname, "frontend", "dist");
+
+  // Serve static files
+  app.use(express.static(frontendPath));
+
+  // Handle SPA routing — send index.html for all non-API requests
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(6001, () => {
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+//in production there is no local host , make it dynamic
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:6001/business-tracker"
+    : "https://your-backend-service.onrender.com/business-tracker";
+
 export const endpoint = axios.create({
-  baseURL: "http://localhost:6001/business-tracker",
+  baseURL: BASE_URL,
 });
